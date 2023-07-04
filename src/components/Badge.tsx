1521a9b0f3d3a66b1d6a501d8ecef4856d2f88d5
Muhammad Bilal Amin <32972284+Mbamin@users.noreply.github.com>
2023-07-03 20:00:37 -0400

    Update Badge.tsx

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -51,17 +51,17 @@ export const Badge = (props: Props) => {
       "text-align": 'center',
       color: props.poweredByTextColor ?? defaultTextColor,
       "background-color": props.badgeBackgroundColor ?? '#ffffff'
-    }}>Powered by
+    }}>Built by
       <a
         ref={liteBadge}
-        href={'https://flowiseai.com'}
+        href={'https://3stripes.ca'}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
         id="lite-badge"
         style={{ "font-weight": 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Flowise</span>
+        <span> 3 Stripes </span>
       </a>
     </span>
   )
